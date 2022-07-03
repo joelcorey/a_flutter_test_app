5edbac5130e499ca45186d77c14d85b2b612c2d9
Joel Corey <joelcorey@fastmail.com>
2022-07-03 13:18:47 -0700

    builder method responsive

---
+++ lib/helpers/responsiveness.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+const int largeScreenSize = 1366;
+const int mediumScreenSize = 768;
+const int smallScreenSize = 360;
+const int customScreenSize = 1100;
+
+class ResponsiveWidget extends StatelessWidget {
+  final Widget largeScreen;
+  final Widget mediumScreen;
+  final Widget smallScreen;
+  final Widget customScreen;
+
+  const ResponsiveWidget(
+      {super.key,
+      required this.largeScreen,
+      required this.mediumScreen,
+      required this.smallScreen,
+      required this.customScreen});
+
+  static bool isSmallScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width < smallScreenSize;
+
+  static bool isMediumScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width >= mediumScreenSize &&
+      MediaQuery.of(context).size.width < largeScreenSize;
+
+  static bool isLargeScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width >= largeScreenSize;
+
+  static bool isCustomScreenSize(BuildContext context) =>
+      MediaQuery.of(context).size.width >= mediumScreenSize &&
+      MediaQuery.of(context).size.width <= customScreenSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      double _width = constraints.maxWidth;
+      if (_width >= largeScreenSize) {
+        return largeScreen;
+      }
+      else if(_width < largeScreenSize && _width >= mediumScreenSize){
+        return mediumScreen ?? largeScreen;
+      } else {
+        return smallScreen ?? largeScreen;
+      }
+    });
+  }
+}
