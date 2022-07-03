c56819a555223a195b21a6abce4bf4f09b62d388
Joel Corey <joelcorey@fastmail.com>
2022-07-03 05:47:49 -0700

    import get in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,19 +10,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.green,
             title: const Text('Derp'),
           ),
           body: Container(
-            child: const Text('Hello WORLD!'),
             margin: const EdgeInsets.all(100),
             padding: const EdgeInsets.all(10),
             height: 100,
             width: 100,
             color: Colors.blue,
+            child: const Text('Hello WORLD!'),
           )),
     );
   }
